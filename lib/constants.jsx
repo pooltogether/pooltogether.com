5580cadb5d2fa0b7cc76db3533d7d1a4f38cea04
Dylan DesRosier <dylan@pooltogether.com>
2021-05-14 16:00:40 -0400

    more deleting

--- lib/constants.jsx
+++ lib/constants.jsx
@@ -1,7 +1,5 @@
 export const SUPPORTED_CHAIN_IDS = [1, 3, 4]
 
-export const CREATOR_ADDRESS = '0xe0f4217390221af47855e094f6e112d43c8698fe'
-
 export const SECONDS_PER_BLOCK = 14
 
 export const DEFAULT_TOKEN_PRECISION = 18
@@ -17,42 +15,6 @@ export const COOKIE_OPTIONS = {
   domain
 }
 
-export const POOLS = [
-  {
-    name: 'DAI Pool',
-    frequency: 'Weekly',
-    symbol: 'PT-cDAI'
-  }
-]
-
-export const PRIZE_STRATEGY_TYPES = {
-  singleRandomWinner: 'singleRandomWinner',
-  multipleWinners: 'multipleWinners'
-}
-
-export const CONTRACT_ADDRESSES = {
-  1: {
-    Usdt: '0xdac17f958d2ee523a2206206994597c13d831ec7'
-    // Dai: '0x6b175474e89094c44da98b954eedeac495271d0f',
-    // PermitAndDepositDai: PermitAndDepositDaiMainnet.address
-  },
-  3: {
-    Usdt: '0x0736d0c130b2ead47476cc262dbed90d7c4eeabd'
-    // Dai: '0xc2118d4d90b274016cb7a54c03ef52e6c537d957',
-    // PermitAndDepositDai: PermitAndDepositDaiRopsten.address
-  },
-  4: {
-    Usdt: '0x3b00ef435fa4fcff5c209a37d1f3dcff37c705ad'
-    // PermitAndDepositDai: PermitAndDepositDaiRinkeby.address,
-  }
-}
-
 export const QUERY_KEYS = {
-  ethereumErc20sQuery: 'ethereumErc20sQuery',
-  ethereumGenericQuery: 'ethereumGenericQuery',
-  poolsQuery: 'poolsQuery',
-  totalPrizePoolPrizes: 'totalPrizePoolPrizes',
-  uniswapTokensQuery: 'uniswapTokensQuery',
-  usePrizePools: 'usePrizePools',
   usePools: 'usePools'
 }
