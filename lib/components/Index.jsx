67e2bb72a9c9b0d630923540895e4633f9f90bca
Charles Bergeron <chuck.bergeron@gmail.com>
2021-09-20 14:28:10 -0700

    * working on mobile hero so it at least renders properly

--- lib/components/Index.jsx
+++ lib/components/Index.jsx
@@ -43,18 +43,15 @@ export const Index = (props) => {
     <>
       {/* mt-24  */}
       <div
-        class='w-full flex flex-col justify-between w-full text-inverse'
+        class='w-full flex flex-col justify-between w-full text-inverse text-center sm:text-left'
         style={{ height: `calc(100vh - ${props.navHeight}px)` }}
       >
-        <div
-          className='pool-container flex flex-col sm:flex-row justify-between mx-auto w-full'
-          style={{ paddingTop: '5vh' }}
-        >
+        <div className='pool-container flex flex-col sm:flex-row justify-between mx-auto w-full pt-2 sm:pt-16'>
           <div className='relative w-full sm:w-5/12 mt-6'>
             <HeroLeft />
           </div>
 
-          <div className='w-full sm:w-1/2'>
+          <div className='w-full sm:w-7/12 lg:w-1/2 mt-6'>
             <HeroRight />
           </div>
         </div>
@@ -71,7 +68,7 @@ export const Index = (props) => {
         <IndexHowItWorks />
         <IndexPoolToken />
         <IndexSecurity />
-        <IndexSupportedBy />
+        {/* <IndexSupportedBy /> */}
         <IndexGetInvolved />
       </div>
     </>
@@ -100,50 +97,20 @@ const DownArrow = () => {
 
 const HeroRight = () => {
   return (
-    <div className='bg-prize-amount text-center pt-8'>
-      <h1 className='text-9xl'>
-        <WeeklyPrizeAmount />
-      </h1>
-      <div className='uppercase font-semibold text-default -mt-2'>In weekly prizes</div>
-      <div className='uppercase font-semibold text-green mt-8 text-xl w-1/2 mx-auto'>
-        Awarded every Thursday!
-      </div>
-    </div>
-  )
-}
-
-const HeroLeft = () => {
-  return (
     <>
-      <span
-        className='font-medium font-inter text-4xl lg:text-6xl text-new-gradient'
-        style={{ lineHeight: 1.125 }}
-      >
-        Win by saving with PoolTogether.
-      </span>
-      <p className='text-accent-1 mt-4' style={{ width: 446 }}>
-        PoolTogether is a crypto-powered savings protocol based on{' '}
-        <a
-          href='https://en.wikipedia.org/wiki/Premium_Bond'
-          target='_blank'
-          title='Wikipedia entry for Premium Bonds'
-        >
-          Premium Bonds
-        </a>
-        . Save your money and have a chance to win prizes every week.
-      </p>
-      <div className='text-xl mt-8'>The more you save, the more you win!</div>
-      <div className='mt-4' style={{ width: 446 }}>
-        <a
-          href='https://app.pooltogether.com'
-          className='new-btn block text-center text-xl w-full capitalize text-xs px-2 py-2 mt-2'
-        >
-          Start saving & winning{' '}
-          <FeatherIcon icon={'chevron-right'} className='inline-block w-6 h-6 -mt-1' />
-        </a>
+      <div className='bg-prize-amount flex flex-col justify-between text-center pt-8'>
+        <div className='pb-10'>
+          <h1 className='text-4xl xs:text-6xl sm:text-9xl -mt-6 sm:-mt-0'>
+            <WeeklyPrizeAmount />
+          </h1>
+          <div className='uppercase font-semibold text-default -mt-2'>In weekly prizes</div>
+        </div>
+        <div className='uppercase font-semibold text-green sm:mt-8 text-xs xs:text-lg lg:text-xl w-1/2 mx-auto'>
+          Awarded every Thursday!
+        </div>
       </div>
 
-      <ul className='flex mt-2'>
+      <ul className='flex justify-center sm:justify-end mt-0 mb-8 sm:mb-0 mx-auto sm:pr-20'>
         <HeroSocialItem width={23} href='https://twitter.com/PoolTogether_'>
           <TwitterIconSvg />
         </HeroSocialItem>
@@ -156,6 +123,42 @@ const HeroLeft = () => {
           <GithubIconSvg />
         </HeroSocialItem>
       </ul>
+    </>
+  )
+}
+
+const HeroLeft = () => {
+  return (
+    <>
+      <span
+        className='font-medium font-inter text-2xl xs:text-3xl lg:text-6xl text-new-gradient'
+        style={{ lineHeight: 1.25 }}
+      >
+        Win by saving with PoolTogether.
+      </span>
+      <p className='text-accent-1 mt-4 sm-max-width-hero'>
+        PoolTogether is a crypto-powered savings protocol based on{' '}
+        <a
+          href='https://en.wikipedia.org/wiki/Premium_Bond'
+          target='_blank'
+          title='Wikipedia entry for Premium Bonds'
+        >
+          Premium Bonds
+        </a>
+        . Save your money and have a chance to win prizes every week.
+      </p>
+      <div className='text-sm xs:text-base sm:text-xl mt-8'>
+        The more you save, the more you win!
+      </div>
+      <div className='mt-4 mb-12 sm:mb-0 sm-max-width-hero'>
+        <a
+          href='https://app.pooltogether.com'
+          className='new-btn block text-center text-xl w-full capitalize text-xs px-2 py-2 mt-2'
+        >
+          Start saving & winning{' '}
+          <FeatherIcon icon={'chevron-right'} className='inline-block w-6 h-6 -mt-1' />
+        </a>
+      </div>
     </>
   )
 }
--- lib/components/IndexSupportedBy.jsx
+++ lib/components/IndexSupportedBy.jsx
@@ -1,104 +1,104 @@
-import React, { useEffect } from 'react'
-import classnames from 'classnames'
-import { useInView } from 'react-intersection-observer'
-import { motion, useAnimation } from 'framer-motion'
+// import React, { useEffect } from 'react'
+// import classnames from 'classnames'
+// import { useInView } from 'react-intersection-observer'
+// import { motion, useAnimation } from 'framer-motion'
 
-import { GridItemSupportedBy } from 'lib/components/GridItemSupportedBy'
+// import { GridItemSupportedBy } from 'lib/components/GridItemSupportedBy'
 
-import ConsensysSvg from 'assets/images/consensys.svg'
-import DTCCapitalSvg from 'assets/images/dtc-capital.svg'
-import IdeoSvg from 'assets/images/ideo.svg'
-import ParaFiSvg from 'assets/images/parafi-logo.svg'
-import MetaCartelSvg from 'assets/images/metacartel-logo.svg'
-import TheLaoSvg from 'assets/images/the-lao-logo.svg'
-import RobotVenturesSvg from 'assets/images/robot-ventures-logo.svg'
+// import ConsensysSvg from 'assets/images/consensys.svg'
+// import DTCCapitalSvg from 'assets/images/dtc-capital.svg'
+// import IdeoSvg from 'assets/images/ideo.svg'
+// import ParaFiSvg from 'assets/images/parafi-logo.svg'
+// import MetaCartelSvg from 'assets/images/metacartel-logo.svg'
+// import TheLaoSvg from 'assets/images/the-lao-logo.svg'
+// import RobotVenturesSvg from 'assets/images/robot-ventures-logo.svg'
 
-export const IndexSupportedBy = () => {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
+// export const IndexSupportedBy = () => {
+//   const controls = useAnimation()
+//   const [ref, inView] = useInView()
 
-  useEffect(() => {
-    if (inView) {
-      controls.start('visible')
-    }
-  }, [controls, inView])
+//   useEffect(() => {
+//     if (inView) {
+//       controls.start('visible')
+//     }
+//   }, [controls, inView])
 
-  const containerVariants = {
-    visible: {
-      transition: {
-        staggerChildren: 0.12
-      }
-    },
-    hidden: {}
-  }
+//   const containerVariants = {
+//     visible: {
+//       transition: {
+//         staggerChildren: 0.12
+//       }
+//     },
+//     hidden: {}
+//   }
 
-  return (
-    <div id='backed-by' className='bg-darkened text-center pt-12 pb-6 sm:pt-20 sm:pb-16'>
-      <div className='pool-container mx-auto'>
-        <h3 className='text-sm xs:text-lg sm:text-xl my-0 sm:mb-12 leading-tight'>
-          Protocol supported by
-        </h3>
+//   return (
+//     <div id='backed-by' className='bg-darkened text-center pt-12 pb-6 sm:pt-20 sm:pb-16'>
+//       <div className='pool-container mx-auto'>
+//         <h3 className='text-sm xs:text-lg sm:text-xl my-0 sm:mb-12 leading-tight'>
+//           Protocol supported by
+//         </h3>
 
-        <motion.div
-          className={classnames(
-            'flex flex-col xs:flex-row xs:flex-wrap justify-start items-start',
-            'mt-2 mb-4 px-4 xs:px-8 rounded-xl -mx-4 sm:-mx-12 lg:-mx-16'
-          )}
-          ref={ref}
-          animate={controls}
-          initial='hidden'
-          variants={containerVariants}
-        >
-          <GridItemSupportedBy
-            title={'ConsenSys'}
-            img={ConsensysSvg}
-            url='https://www.consensys.com'
-          />
+//         <motion.div
+//           className={classnames(
+//             'flex flex-col xs:flex-row xs:flex-wrap justify-start items-start',
+//             'mt-2 mb-4 px-4 xs:px-8 rounded-xl -mx-4 sm:-mx-12 lg:-mx-16'
+//           )}
+//           ref={ref}
+//           animate={controls}
+//           initial='hidden'
+//           variants={containerVariants}
+//         >
+//           <GridItemSupportedBy
+//             title={'ConsenSys'}
+//             img={ConsensysSvg}
+//             url='https://www.consensys.com'
+//           />
 
-          <GridItemSupportedBy
-            title={'IDEO'}
-            img={IdeoSvg}
-            url='https://www.ideo.com'
-            maxHeight={30}
-          />
+//           <GridItemSupportedBy
+//             title={'IDEO'}
+//             img={IdeoSvg}
+//             url='https://www.ideo.com'
+//             maxHeight={30}
+//           />
 
-          <GridItemSupportedBy
-            title={'DTC Capital'}
-            img={DTCCapitalSvg}
-            url='https://www.dtc.capital'
-          />
+//           <GridItemSupportedBy
+//             title={'DTC Capital'}
+//             img={DTCCapitalSvg}
+//             url='https://www.dtc.capital'
+//           />
 
-          <GridItemSupportedBy title={'ParaFi'} img={ParaFiSvg} url='https://www.parafi.capital/' />
+//           <GridItemSupportedBy title={'ParaFi'} img={ParaFiSvg} url='https://www.parafi.capital/' />
 
-          <GridItemSupportedBy
-            title={'MetaCartel Ventures'}
-            img={MetaCartelSvg}
-            url='https://metacartel.org'
-          />
+//           <GridItemSupportedBy
+//             title={'MetaCartel Ventures'}
+//             img={MetaCartelSvg}
+//             url='https://metacartel.org'
+//           />
 
-          <GridItemSupportedBy title={'The LAO'} img={TheLaoSvg} url='https://thelao.io' />
+//           <GridItemSupportedBy title={'The LAO'} img={TheLaoSvg} url='https://thelao.io' />
 
-          <GridItemSupportedBy
-            title={'Robot Ventures'}
-            img={RobotVenturesSvg}
-            url='https://twitter.com/robotventures'
-          />
-        </motion.div>
+//           <GridItemSupportedBy
+//             title={'Robot Ventures'}
+//             img={RobotVenturesSvg}
+//             url='https://twitter.com/robotventures'
+//           />
+//         </motion.div>
 
-        {/* <div className='flex justify-center items-center sm:-mx-2'>
-        <a
-          href='https://makerdao.com'
-          title='View the Maker site'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            src={MakerDaoSvg}
-            className='mt-4'
-          />
-        </a>
-      </div> */}
-      </div>
-    </div>
-  )
-}
+//         {/* <div className='flex justify-center items-center sm:-mx-2'>
+//         <a
+//           href='https://makerdao.com'
+//           title='View the Maker site'
+//           target='_blank'
+//           rel='noopener noreferrer'
+//         >
+//           <img
+//             src={MakerDaoSvg}
+//             className='mt-4'
+//           />
+//         </a>
+//       </div> */}
+//       </div>
+//     </div>
+//   )
+// }
