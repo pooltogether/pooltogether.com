1d032fd21d8b97b554f29ab129a1b8d0b2a31fd8
Charles Bergeron <chuck.bergeron@gmail.com>
2021-12-16 08:55:34 -0800

    * fix faq link

--- lib/components/Footer.jsx
+++ lib/components/Footer.jsx
@@ -60,7 +60,7 @@ export const Footer = () => {
                 <ul>
                   <FooterLink href='/developers'>Developers</FooterLink>
                   <FooterLink href='/audits'>Security</FooterLink>
-                  <FooterLink href='/faq'>FAQ</FooterLink>
+                  <FooterLink href='https://docs.pooltogether.com/faq/general'>FAQ</FooterLink>
                   <FooterLink href='/brand-assets'>Assets</FooterLink>
                   <FooterLink href='https://gov.pooltogether.com/'>Governance</FooterLink>
                   <FooterLink href='https://www.notion.so/PoolTogether-Knowledge-Base-fa721ccefa3242eaabd125a8415acd27'>
