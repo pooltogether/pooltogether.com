fe5288fa60fea559ee65baefdf7f51231f80e812
Dylan DesRosier <dylan@pooltogether.com>
2022-03-21 10:36:13 -0400

    Fix safari video player

--- lib/components/NftVideoPlayer.jsx
+++ lib/components/NftVideoPlayer.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
 
-export const NftVideoPlayer = ({
-  setIndexPlaying,
-  indexPlaying,
-  label,
-  dropRate,
-  files,
-  isPlaying
-}) => {
+export const NftVideoPlayer = ({ label, dropRate, files, isPlaying }) => {
   return (
     <div>
-      <ReactPlayer url={files} loop playing={isPlaying} volume={0} width='100vw' height='50vh' />
+      <VideoPlayer files={files} isPlaying={isPlaying} />
       <div className='text-white text-center bg-pt-purple-bright py-2 sm:py-4'>
         <span className='font-semibold'>{label}</span>
         <span className='block text-xxs opacity-60'>{dropRate}% drop rate</span>
       </div>
+    </div>
+  )
+}
+
+export const VideoPlayer = (props) => {
+  const { isPlaying, files } = props
+
+  return (
+    <div className='flex w-screen' style={{ height: '50vh' }}>
+      {isPlaying && (
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          preload='auto'
+          className='mx-auto'
+          style={{ height: 'inherit' }}
+        >
+          <source src={files.webm} type='video/webm' />
+          <source src={files.mp4} type='video/mp4' />
+        </video>
+      )}
     </div>
   )
 }
