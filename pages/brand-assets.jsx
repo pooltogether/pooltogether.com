1abcf9b7d3d8b90dc723ac9607378458dddbbd8f
Dylan DesRosier <dylan@pooltogether.com>
2022-04-12 11:14:34 -0400

    Update all other pages

--- pages/brand-assets.jsx
+++ pages/brand-assets.jsx
@@ -4,6 +4,19 @@ import { Layout } from 'lib/components/Layout'
 import { PageContainer } from 'lib/components/PageContainer'
 import { BrandAssets } from 'lib/components/BrandAssets'
 
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import i18nConfig from '../next-i18next.config'
+
+export async function getStaticProps(context) {
+  const { locale } = context
+  const translations = await serverSideTranslations(locale, ['common'], i18nConfig)
+  return {
+    props: {
+      ...translations
+    }
+  }
+}
+
 const BrandAssetsPage = () => {
   return (
     <Layout>
