672496e65f1dfd29936461804ebe71008d0f0cf6
Dylan DesRosier <dylan@pooltogether.com>
2022-09-21 14:35:59 -0400

    Update prize amount

--- lib/components/WeeklyPrizeAmount.jsx
+++ lib/components/WeeklyPrizeAmount.jsx
@@ -1,32 +1,21 @@
 import React from 'react'
 import { ThemedClipSpinner } from '@pooltogether/react-components'
-
-import { numberWithCommas } from 'lib/utils/numberWithCommas'
-import { usePooltogetherTotalPrizes } from 'lib/hooks/usePooltogetherTvl'
+import { numberWithCommas } from '@pooltogether/utilities'
+import { useQuery } from 'react-query'
 
 export const WeeklyPrizeAmount = (props) => {
-  const totalPrizes = usePooltogetherTotalPrizes()
-
-  const formatNumbers = (num) => {
-    if (num > 1000000) {
-      const isOneMillion = `${num / 1000000}`.substr(0, 4) === '1.00'
-      return `$${numberWithCommas(num / 1000000, { precision: isOneMillion ? 0 : 2 })} ${'million'}`
-    } else if (num >= 10000) {
-      return `$${numberWithCommas(num, { precision: 0 })}`
-    } else {
-      return `$${numberWithCommas(num, { precision: 2 })}`
-    }
-  }
+  const { data: amount, isFetched } = useWeeklyPrizeAmount()
 
   // Check if data has loaded
-  if (totalPrizes === null) {
+  if (!isFetched) {
     return <ThemedClipSpinner />
   }
 
-  // add hard-coded V4 amount (as of apr 11, 2021) over 1 week:
-  // $11,160 * 7
-  // 78120
-  const totalV3AndV4PrizesWeekly = Number(totalPrizes) + 78120
+  return <b>${amount}</b>
+}
 
-  return <b>{formatNumbers(totalV3AndV4PrizesWeekly)}</b>
+const useWeeklyPrizeAmount = () => {
+  return useQuery('weeklyPrizeAmount', () => {
+    return numberWithCommas('29400', { precision: 0 })
+  })
 }
