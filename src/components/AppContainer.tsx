e1cea4549bf8cc2461ae3fec15bae621628d29a4
Ncookiez <ncukiert@gmail.com>
2025-04-23 14:15:46 -0400

    bumped next-intl to fix netlify build

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,35 +1,35 @@
-import { useFathom } from "@shared/generic-react-hooks";
-import { Flowbite } from "@shared/ui";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { NextIntlProvider } from "next-intl";
-import { AppProps } from "next/app";
-import { useRouter } from "next/router";
+import { useFathom } from '@shared/generic-react-hooks'
+import { Flowbite } from '@shared/ui'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { IntlProvider } from 'next-intl'
+import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 // React Query Client:
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export const AppContainer = (props: AppProps) => {
-  const { Component, pageProps } = props;
+  const { Component, pageProps } = props
 
-  const router = useRouter();
+  const router = useRouter()
 
   // Fathom Analytics
   useFathom(
     process.env.NEXT_PUBLIC_FATHOM_SITE_ID as string,
-    ["pooltogether.com"],
+    ['pooltogether.com'],
     // @ts-ignore
     router.events?.on,
     router.events?.off
-  );
+  )
 
   return (
     <Flowbite theme={{ dark: true }}>
       <QueryClientProvider client={queryClient}>
-        <NextIntlProvider messages={pageProps.messages}>
-          <div id="modal-root" />
+        <IntlProvider locale={router.locale ?? 'en'} messages={pageProps.messages}>
+          <div id='modal-root' />
           <Component {...pageProps} />
-        </NextIntlProvider>
+        </IntlProvider>
       </QueryClientProvider>
     </Flowbite>
-  );
-};
+  )
+}
